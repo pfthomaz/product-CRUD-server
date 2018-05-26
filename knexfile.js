c78e99c587b2560b02acf6c2e367b923fcf04d99
Pedro F Thomaz <pfthomaz@gmail.com>
2018-05-26 16:16:23 +0100

    final commit

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,18 @@
-
 module.exports = {
+  // development: {
+  //   client: 'pg',
+  //   connection: {
+  //     host: 'localhost',
+  //     user: 'postgres',
+  //     password: 'postgres',
+  //     database: 'cjs_store'
+  //   }
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/cjs_store'
+    connection: 'postgresql://postgres:postgres@localhost/cjs_store'
   },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
   },
-};
+}
